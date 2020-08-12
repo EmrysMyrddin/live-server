981c9e842382bac32b2d9de5976eefa33e50a696
Valentin Cocaud <v.cocaud@gmail.com>
2020-08-12 17:34:08 +0200

    :pushpin: pin versions to avoid inconsistent install

--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var path = require('path');
 var exec = require('child_process').execFile;
-var cmd = path.join(__dirname, "..", "live-server.js");
+var cmd = require.resolve("../live-server.js");
 var opts = {
 	timeout: 2000,
 	maxBuffer: 1024
@@ -15,39 +15,39 @@ function exec_test(args, callback) {
 
 describe('command line usage', function() {
 	it('--version', function(done) {
-		exec_test([ "--version" ], function(error, stdout, stdin) {
+		exec_test([ "--version" ], function(error, stdout) {
 			assert(!error, error);
-			assert(stdout.indexOf("live-server") === 0, "version not found");
+			assert(stdout.indexOf("@emrys-myrddin/live-server") === 0, "version not found");
 			done();
 		});
 	});
 	it('--help', function(done) {
-		exec_test([ "--help" ], function(error, stdout, stdin) {
+		exec_test([ "--help" ], function(error, stdout) {
 			assert(!error, error);
 			assert(stdout.indexOf("Usage: live-server") === 0, "usage not found");
 			done();
 		});
 	});
 	it('--quiet', function(done) {
-		exec_test([ "--quiet", "--no-browser", "--test" ], function(error, stdout, stdin) {
+		exec_test([ "--quiet", "--no-browser", "--test" ], function(error, stdout) {
 			assert(!error, error);
 			assert(stdout === "", "stdout not empty");
 			done();
 		});
 	});
 	it('--port', function(done) {
-		exec_test([ "--port=16123", "--no-browser", "--test" ], function(error, stdout, stdin) {
+		exec_test([ "--port=16123", "--no-browser", "--test" ], function(error, stdout) {
 			assert(!error, error);
 			assert(stdout.indexOf("Serving") >= 0, "serving string not found");
-			assert(stdout.indexOf("at http://127.0.0.1:16123") != -1, "port string not found");
+			assert(stdout.indexOf("at http://127.0.0.1:16123") !== -1, "port string not found");
 			done();
 		});
 	});
 	it('--host', function(done) {
-		exec_test([ "--host=localhost", "--no-browser", "--test" ], function(error, stdout, stdin) {
+		exec_test([ "--host=localhost", "--no-browser", "--test" ], function(error, stdout) {
 			assert(!error, error);
 			assert(stdout.indexOf("Serving") >= 0, "serving string not found");
-			assert(stdout.indexOf("at http://localhost:") != -1, "host string not found");
+			assert(stdout.indexOf("at http://localhost:") !== -1, "host string not found");
 			done();
 		});
 	});
@@ -56,7 +56,7 @@ describe('command line usage', function() {
 			[ "--htpasswd=" + path.join(__dirname, "data/htpasswd-test"),
 				"--no-browser",
 				"--test"
-			], function(error, stdout, stdin) {
+			], function(error, stdout) {
 			assert(!error, error);
 			assert(stdout.indexOf("Serving") >= 0, "serving string not found");
 			done();
